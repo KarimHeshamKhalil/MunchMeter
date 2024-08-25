dfa6afc876cd6c027cdc615dd84d5ac460bd40a1
KarimHeshamKhalil <128839713+KarimHeshamKhalil@users.noreply.github.com>
2024-08-25 12:40:01 +0300

    modified padding and text size on different screen sizes

--- src/Root.jsx
+++ src/Root.jsx
@@ -13,7 +13,7 @@ export default function Root() {
     <div>
       <Navbar />
 
-      <div className='flex items-center justify-center checkPoint1:justify-between flex-col-reverse checkPoint1:flex-row-reverse px-16 h-[105vh] text-center checkPoint1:text-left text-green1 bg-slate-100 relative'>
+      <div className='flex items-center justify-center checkPoint1:justify-between flex-col-reverse gap-16 checkPoint1:flex-row-reverse px-16 h-[110vh] text-center checkPoint1:text-left text-green1 bg-slate-100 relative'>
          <div className='opacity-60 relative top-20 checkPoint1:-top-4 w-[200px] md:w-[300px] block checkPoint1:block'>
           <img src={basil} />
          </div>
@@ -42,10 +42,10 @@ export default function Root() {
       </div>
 
       <div className='relative'>
-        <div className='px-16 py-16 mb-4 bg-slate-100 flex flex-col checkPoint2:flex-row items-center justify-center checkPoint2:justify-between'>
+        <div className='px-4 checkPoint2:px-12 py-16 mb-4 bg-slate-100 flex flex-col checkPoint2:flex-row items-center justify-center checkPoint2:justify-between'>
           <div className='px-6 py-6 rounded-md bg-slate-100 relative hover:-translate-y-4 hover:shadow-card transition-all duration-200'>
-            <h3 className='text-3xl font-medium mb-3 text-green1 flex items-center gap-2 rubik'><MdFeedback /> Collecting Feedback</h3>
-            <p className='max-w-[450px] bigCheckPoint:max-w-[600px] text-xl text-green1'>
+            <h3 className='text-2xl checkPoint2:text-3xl font-medium mb-3 text-green1 flex items-center gap-2 rubik'><MdFeedback /> Collecting Feedback</h3>
+            <p className='max-w-[450px] bigCheckPoint:max-w-[600px] text-lg checkPoint2:text-xl text-green1'>
               We gathered valuable <strong>insights</strong> and <strong>feedback</strong> from real users and expert contributors, addressing their problems and incorporating their ideas into the <strong>app's design</strong> and <strong>functionality</strong>.
             </p>
           </div>
@@ -54,11 +54,11 @@ export default function Root() {
             <img src={chartoonPic1} />
           </div>
         </div>
-        <div className='px-12 py-16 mb-4 bg-slate-100 flex flex-col checkPoint2:flex-row-reverse items-center justify-center checkPoint2:justify-between'>
+        <div className='px-4 checkPoin2:px-12 py-16 mb-4 bg-slate-100 flex flex-col checkPoint2:flex-row-reverse items-center justify-center checkPoint2:justify-between'>
           <div className='px-6 py-6 rounded-md bg-slate-100 relative hover:-translate-y-4 hover:shadow-card transition-all duration-200'>
-            <h3 className='text-3xl font-medium mb-3 text-green1 flex items-center gap-2 rubik'><MdFeedback />Identifying Problems</h3>
-            <p className='max-w-[450px] bigCheckPoint:max-w-[600px] text-xl text-green1'>
-            We've identified the common <strong>challenges</strong> people face when it comes to <strong>nutrition</strong> and maintaining a healthy diet. Our goal is to address these issues by providing <strong>practical</strong>, <strong>personalized</strong> solutions.
+            <h3 className='text-2xl checkPoint2:text-3xl font-medium mb-3 text-green1 flex items-center gap-2 rubik'><MdFeedback />Identifying Problems</h3>
+            <p className='max-w-[450px] bigCheckPoint:max-w-[600px] text-lg checkPoint2:text-xl text-green1'>
+              We've identified the common <strong>challenges</strong> people face when it comes to <strong>nutrition</strong> and maintaining a healthy diet. Our goal is to address these issues by providing <strong>practical</strong>, <strong>personalized</strong> solutions.
             </p>
           </div>
 
@@ -66,10 +66,10 @@ export default function Root() {
             <img src='https://cdni.iconscout.com/illustration/premium/thumb/problem-solving-illustration-download-in-svg-png-gif-file-formats--looking-for-solution-business-idea-and-pack-people-illustrations-4019071.png' />
           </div>
         </div>
-        <div className='px-12 py-16 bg-slate-100 items-center justify-center flex flex-col checkPoint2:flex-row checkPoint2:justify-between mb-12'>
+        <div className='px-4 checkPoint2:px-12 py-16 bg-slate-100 items-center justify-center flex flex-col checkPoint2:flex-row checkPoint2:justify-between mb-12'>
           <div className='px-6 py-6 rounded-md bg-slate-100 relative hover:-translate-y-4 hover:shadow-card transition-all duration-200'>
-            <h3 className='text-3xl font-medium mb-3 text-green1 flex items-center gap-2 rubik'><FaLightbulb />Best Solution</h3>
-            <p className='max-w-[450px] bigCheckPoint:max-w-[600px] text-xl text-green1'>
+            <h3 className='text-2xl checkPoint2:text-3xl font-medium mb-3 text-green1 flex items-center gap-2 rubik'><FaLightbulb />Best Solution</h3>
+            <p className='max-w-[450px] bigCheckPoint:max-w-[600px] text-lg checkPoint2:text-xl text-green1'>
               We have thought of <strong>three</strong> possible solutions and we have decided to create a <strong>Web App</strong> so it is <strong>suitable</strong> for everyone and <strong>scalable</strong>
             </p>
           </div>
