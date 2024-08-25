1d57c54cbd1eb8fe4394e2d9cc7ed147c1f61ec9
KarimHeshamKhalil <128839713+KarimHeshamKhalil@users.noreply.github.com>
2024-08-25 19:18:20 +0300

    new form final change

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -203,7 +203,7 @@ export default function Sidebar({ setFoodLog, setActivityLog }) {
         isShown && !isBurnCalories && !isConsume && (
           <div className='fixed inset-0 w-[600px] height-[100vh] z-50'>
             <div onClick={toggleModal} className='fixed inset-0 w-[1000vh] height-[100vh] bg-modal z-100'></div>
-            <div className='modal-content shadow-innerborder overflow-clip border border-green2 bg-slate-100 rounded-lg px-6  py-6'>
+            <div className='modal-content w-[400px] shadow-innerborder overflow-clip border border-green2 bg-slate-100 rounded-lg px-6  py-6'>
               <button onClick={toggleModal} className='absolute -top-1 -right-1 px-2 pr-3 py-2 pt-3 bg-green2 borer border-green2 text-white rounded-md hover:text-green2 hover:bg-slate-50 transition-all duration-100 shadow-shadow1'>
                 <IoCloseOutline />
               </button>
